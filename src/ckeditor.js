15f940dd37b9935471c8f3daa22d7135ef4f08e9
Yahav <yahav.shasha@gmail.com>
2019-10-17 17:00:56 +0000

    Full and Mini editors setup

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -9,17 +9,30 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
+import Font from '@ckeditor/ckeditor5-font/src/font';
+
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -28,24 +41,41 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+// export default class ClassicEditor extends ClassicEditorBase {}
+class FullEditor extends ClassicEditorBase {
+}
+
+class MiniEditor extends ClassicEditorBase {
+}
 
 // Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
+FullEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
+
 	Bold,
 	Italic,
+	Underline,
+	Code,
+	Font,
+
+	Indent,
+	IndentBlock,
+
+	Alignment,
+
 	BlockQuote,
 	CKFinder,
-	EasyImage,
 	Heading,
+
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
+	ImageResize,
+
 	Link,
 	List,
 	MediaEmbed,
@@ -55,25 +85,204 @@ ClassicEditor.builtinPlugins = [
 	TableToolbar
 ];
 
+MiniEditor.builtinPlugins = [
+	Essentials,
+	UploadAdapter,
+	Autoformat,
+
+	Bold,
+	Italic,
+	Underline,
+
+	Indent,
+	IndentBlock,
+
+	Link,
+
+	Paragraph,
+];
+
 // Editor configuration.
-ClassicEditor.defaultConfig = {
+FullEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
+
 			'|',
+
+			'alignment',
+
+			'|',
+
 			'bold',
 			'italic',
-			'link',
+			'underline',
+
+			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
+
+			'|',
+
+			'outdent',
+			'indent',
+
+			'|',
+
 			'bulletedList',
 			'numberedList',
-			'imageUpload',
+
+			'|',
+
 			'blockQuote',
 			'insertTable',
+			'link',
+
+			'|',
+
+			'ckfinder',
 			'mediaEmbed',
+
+			'|',
+
 			'undo',
 			'redo'
 		]
 	},
+
+	fontSize: {
+		options: [
+			9,
+			11,
+			13,
+			'default',
+			17,
+			19,
+			21
+		]
+	},
+
+	fontFamily: {
+		options: [
+			'default',
+			'Ubuntu, Arial, sans-serif',
+			'Ubuntu Mono, Courier New, Courier, monospace'
+		]
+	},
+
+	fontColor: {
+		colors: [ 'hsl(0,0%,0%)', 'hsl(0,0%,12.5%)', 'hsl(0,0%,25%)', 'hsl(0,0%,37.5%)',
+			'hsl(0,0%,50%)', 'hsl(0,0%,62.50%)', 'hsl(0,0%,75%)', 'hsl(0,0%,87.5%)', {
+				color: 'hsl(0,0%,100%)',
+				hasBorder: !0
+			}, 'hsl(0,100%,10%)', 'hsl(0,100%,20%)', 'hsl(0,100%,30%)', 'hsl(0,100%,40%)',
+			'hsl(0,100%,50%)', 'hsl(0,100%,60%)', 'hsl(0,100%,70%)', 'hsl(0,100%,80%)', {
+				color: 'hsl(0,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(30,100%,10%)', 'hsl(30,100%,20%)', 'hsl(30,100%,30%)', 'hsl(30,100%,40%)',
+			'hsl(30,100%,50%)', 'hsl(30,100%,60%)', 'hsl(30,100%,70%)', 'hsl(30,100%,80%)', {
+				color: 'hsl(30,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(60,100%,10%)', 'hsl(60,100%,20%)', 'hsl(60,100%,30%)', 'hsl(60,100%,40%)',
+			'hsl(60,100%,50%)', 'hsl(60,100%,60%)', 'hsl(60,100%,70%)', 'hsl(60,100%,80%)', {
+				color: 'hsl(60,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(90,100%,10%)', 'hsl(90,100%,20%)', 'hsl(90,100%,30%)', 'hsl(90,100%,40%)',
+			'hsl(90,100%,50%)', 'hsl(90,100%,60%)', 'hsl(90,100%,70%)', 'hsl(90,100%,80%)', {
+				color: 'hsl(90,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(120,100%,10%)', 'hsl(120,100%,20%)', 'hsl(120,100%,30%)', 'hsl(120,100%,40%)',
+			'hsl(120,100%,50%)', 'hsl(120,100%,60%)', 'hsl(120,100%,70%)', 'hsl(120,100%,80%)', {
+				color: 'hsl(120,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(150,100%,10%)', 'hsl(150,100%,20%)', 'hsl(150,100%,30%)', 'hsl(150,100%,40%)',
+			'hsl(150,100%,50%)', 'hsl(150,100%,60%)', 'hsl(150,100%,70%)', 'hsl(150,100%,80%)', {
+				color: 'hsl(150,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(180,100%,10%)', 'hsl(180,100%,20%)', 'hsl(180,100%,30%)', 'hsl(180,100%,40%)',
+			'hsl(180,100%,50%)', 'hsl(180,100%,60%)', 'hsl(180,100%,70%)', 'hsl(180,100%,80%)', {
+				color: 'hsl(180,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(210,100%,10%)', 'hsl(210,100%,20%)', 'hsl(210,100%,30%)', 'hsl(210,100%,40%)',
+			'hsl(210,100%,50%)', 'hsl(210,100%,60%)', 'hsl(210,100%,70%)', 'hsl(210,100%,80%)', {
+				color: 'hsl(210,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(240,100%,10%)', 'hsl(240,100%,20%)', 'hsl(240,100%,30%)', 'hsl(240,100%,40%)',
+			'hsl(240,100%,50%)', 'hsl(240,100%,60%)', 'hsl(240,100%,70%)', 'hsl(240,100%,80%)', {
+				color: 'hsl(240,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(270,100%,10%)', 'hsl(270,100%,20%)', 'hsl(270,100%,30%)', 'hsl(270,100%,40%)',
+			'hsl(270,100%,50%)', 'hsl(270,100%,60%)', 'hsl(270,100%,70%)', 'hsl(270,100%,80%)', {
+				color: 'hsl(270,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(300,100%,10%)', 'hsl(300,100%,20%)', 'hsl(300,100%,30%)', 'hsl(300,100%,40%)',
+			'hsl(300,100%,50%)', 'hsl(300,100%,60%)', 'hsl(300,100%,70%)', 'hsl(300,100%,80%)', {
+				color: 'hsl(300,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(330,100%,10%)', 'hsl(330,100%,20%)', 'hsl(330,100%,30%)', 'hsl(330,100%,40%)',
+			'hsl(330,100%,50%)', 'hsl(330,100%,60%)', 'hsl(330,100%,70%)', 'hsl(330,100%,80%)', {
+				color: 'hsl(330,100%,90%)',
+				hasBorder: !0
+			} ], columns: 9, documentColors: 18
+	},
+
+	fontBackgroundColor: {
+		colors: [ 'hsl(0,0%,0%)', 'hsl(0,0%,12.5%)', 'hsl(0,0%,25%)', 'hsl(0,0%,37.5%)',
+			'hsl(0,0%,50%)', 'hsl(0,0%,62.50%)', 'hsl(0,0%,75%)', 'hsl(0,0%,87.5%)', {
+				color: 'hsl(0,0%,100%)',
+				hasBorder: !0
+			}, 'hsl(0,100%,10%)', 'hsl(0,100%,20%)', 'hsl(0,100%,30%)', 'hsl(0,100%,40%)',
+			'hsl(0,100%,50%)', 'hsl(0,100%,60%)', 'hsl(0,100%,70%)', 'hsl(0,100%,80%)', {
+				color: 'hsl(0,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(30,100%,10%)', 'hsl(30,100%,20%)', 'hsl(30,100%,30%)', 'hsl(30,100%,40%)',
+			'hsl(30,100%,50%)', 'hsl(30,100%,60%)', 'hsl(30,100%,70%)', 'hsl(30,100%,80%)', {
+				color: 'hsl(30,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(60,100%,10%)', 'hsl(60,100%,20%)', 'hsl(60,100%,30%)', 'hsl(60,100%,40%)',
+			'hsl(60,100%,50%)', 'hsl(60,100%,60%)', 'hsl(60,100%,70%)', 'hsl(60,100%,80%)', {
+				color: 'hsl(60,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(90,100%,10%)', 'hsl(90,100%,20%)', 'hsl(90,100%,30%)', 'hsl(90,100%,40%)',
+			'hsl(90,100%,50%)', 'hsl(90,100%,60%)', 'hsl(90,100%,70%)', 'hsl(90,100%,80%)', {
+				color: 'hsl(90,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(120,100%,10%)', 'hsl(120,100%,20%)', 'hsl(120,100%,30%)', 'hsl(120,100%,40%)',
+			'hsl(120,100%,50%)', 'hsl(120,100%,60%)', 'hsl(120,100%,70%)', 'hsl(120,100%,80%)', {
+				color: 'hsl(120,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(150,100%,10%)', 'hsl(150,100%,20%)', 'hsl(150,100%,30%)', 'hsl(150,100%,40%)',
+			'hsl(150,100%,50%)', 'hsl(150,100%,60%)', 'hsl(150,100%,70%)', 'hsl(150,100%,80%)', {
+				color: 'hsl(150,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(180,100%,10%)', 'hsl(180,100%,20%)', 'hsl(180,100%,30%)', 'hsl(180,100%,40%)',
+			'hsl(180,100%,50%)', 'hsl(180,100%,60%)', 'hsl(180,100%,70%)', 'hsl(180,100%,80%)', {
+				color: 'hsl(180,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(210,100%,10%)', 'hsl(210,100%,20%)', 'hsl(210,100%,30%)', 'hsl(210,100%,40%)',
+			'hsl(210,100%,50%)', 'hsl(210,100%,60%)', 'hsl(210,100%,70%)', 'hsl(210,100%,80%)', {
+				color: 'hsl(210,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(240,100%,10%)', 'hsl(240,100%,20%)', 'hsl(240,100%,30%)', 'hsl(240,100%,40%)',
+			'hsl(240,100%,50%)', 'hsl(240,100%,60%)', 'hsl(240,100%,70%)', 'hsl(240,100%,80%)', {
+				color: 'hsl(240,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(270,100%,10%)', 'hsl(270,100%,20%)', 'hsl(270,100%,30%)', 'hsl(270,100%,40%)',
+			'hsl(270,100%,50%)', 'hsl(270,100%,60%)', 'hsl(270,100%,70%)', 'hsl(270,100%,80%)', {
+				color: 'hsl(270,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(300,100%,10%)', 'hsl(300,100%,20%)', 'hsl(300,100%,30%)', 'hsl(300,100%,40%)',
+			'hsl(300,100%,50%)', 'hsl(300,100%,60%)', 'hsl(300,100%,70%)', 'hsl(300,100%,80%)', {
+				color: 'hsl(300,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(330,100%,10%)', 'hsl(330,100%,20%)', 'hsl(330,100%,30%)', 'hsl(330,100%,40%)',
+			'hsl(330,100%,50%)', 'hsl(330,100%,60%)', 'hsl(330,100%,70%)', 'hsl(330,100%,80%)', {
+				color: 'hsl(330,100%,90%)',
+				hasBorder: !0
+			} ], columns: 9, documentColors: 18
+	},
+
 	image: {
 		toolbar: [
 			'imageStyle:full',
@@ -82,6 +291,7 @@ ClassicEditor.defaultConfig = {
 			'imageTextAlternative'
 		]
 	},
+
 	table: {
 		contentToolbar: [
 			'tableColumn',
@@ -89,6 +299,96 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+
+	link: {
+		// Automatically add target="_blank" and rel="noopener noreferrer" to all external links.
+		addTargetToExternalLinks: true,
+	},
+
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
+};
+
+MiniEditor.defaultConfig = {
+	toolbar: {
+		items: [
+
+			'bold',
+			'italic',
+			'underline',
+			'fontColor',
+
+			'|',
+
+			'link',
+
+			'|',
+
+			'outdent',
+			'indent',
+
+		]
+	},
+	fontColor: {
+		colors: [ 'hsl(0,0%,0%)', 'hsl(0,0%,12.5%)', 'hsl(0,0%,25%)', 'hsl(0,0%,37.5%)',
+			'hsl(0,0%,50%)', 'hsl(0,0%,62.50%)', 'hsl(0,0%,75%)', 'hsl(0,0%,87.5%)', {
+				color: 'hsl(0,0%,100%)',
+				hasBorder: !0
+			}, 'hsl(0,100%,10%)', 'hsl(0,100%,20%)', 'hsl(0,100%,30%)', 'hsl(0,100%,40%)',
+			'hsl(0,100%,50%)', 'hsl(0,100%,60%)', 'hsl(0,100%,70%)', 'hsl(0,100%,80%)', {
+				color: 'hsl(0,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(30,100%,10%)', 'hsl(30,100%,20%)', 'hsl(30,100%,30%)', 'hsl(30,100%,40%)',
+			'hsl(30,100%,50%)', 'hsl(30,100%,60%)', 'hsl(30,100%,70%)', 'hsl(30,100%,80%)', {
+				color: 'hsl(30,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(60,100%,10%)', 'hsl(60,100%,20%)', 'hsl(60,100%,30%)', 'hsl(60,100%,40%)',
+			'hsl(60,100%,50%)', 'hsl(60,100%,60%)', 'hsl(60,100%,70%)', 'hsl(60,100%,80%)', {
+				color: 'hsl(60,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(90,100%,10%)', 'hsl(90,100%,20%)', 'hsl(90,100%,30%)', 'hsl(90,100%,40%)',
+			'hsl(90,100%,50%)', 'hsl(90,100%,60%)', 'hsl(90,100%,70%)', 'hsl(90,100%,80%)', {
+				color: 'hsl(90,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(120,100%,10%)', 'hsl(120,100%,20%)', 'hsl(120,100%,30%)', 'hsl(120,100%,40%)',
+			'hsl(120,100%,50%)', 'hsl(120,100%,60%)', 'hsl(120,100%,70%)', 'hsl(120,100%,80%)', {
+				color: 'hsl(120,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(150,100%,10%)', 'hsl(150,100%,20%)', 'hsl(150,100%,30%)', 'hsl(150,100%,40%)',
+			'hsl(150,100%,50%)', 'hsl(150,100%,60%)', 'hsl(150,100%,70%)', 'hsl(150,100%,80%)', {
+				color: 'hsl(150,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(180,100%,10%)', 'hsl(180,100%,20%)', 'hsl(180,100%,30%)', 'hsl(180,100%,40%)',
+			'hsl(180,100%,50%)', 'hsl(180,100%,60%)', 'hsl(180,100%,70%)', 'hsl(180,100%,80%)', {
+				color: 'hsl(180,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(210,100%,10%)', 'hsl(210,100%,20%)', 'hsl(210,100%,30%)', 'hsl(210,100%,40%)',
+			'hsl(210,100%,50%)', 'hsl(210,100%,60%)', 'hsl(210,100%,70%)', 'hsl(210,100%,80%)', {
+				color: 'hsl(210,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(240,100%,10%)', 'hsl(240,100%,20%)', 'hsl(240,100%,30%)', 'hsl(240,100%,40%)',
+			'hsl(240,100%,50%)', 'hsl(240,100%,60%)', 'hsl(240,100%,70%)', 'hsl(240,100%,80%)', {
+				color: 'hsl(240,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(270,100%,10%)', 'hsl(270,100%,20%)', 'hsl(270,100%,30%)', 'hsl(270,100%,40%)',
+			'hsl(270,100%,50%)', 'hsl(270,100%,60%)', 'hsl(270,100%,70%)', 'hsl(270,100%,80%)', {
+				color: 'hsl(270,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(300,100%,10%)', 'hsl(300,100%,20%)', 'hsl(300,100%,30%)', 'hsl(300,100%,40%)',
+			'hsl(300,100%,50%)', 'hsl(300,100%,60%)', 'hsl(300,100%,70%)', 'hsl(300,100%,80%)', {
+				color: 'hsl(300,100%,90%)',
+				hasBorder: !0
+			}, 'hsl(330,100%,10%)', 'hsl(330,100%,20%)', 'hsl(330,100%,30%)', 'hsl(330,100%,40%)',
+			'hsl(330,100%,50%)', 'hsl(330,100%,60%)', 'hsl(330,100%,70%)', 'hsl(330,100%,80%)', {
+				color: 'hsl(330,100%,90%)',
+				hasBorder: !0
+			} ], columns: 9, documentColors: 18
+	},
+
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'en'
+};
+
+export default {
+	FullEditor, MiniEditor
 };
